f01881018caf5ccb0cfbab3726896f0a8d8f3d27
Poonam Tindwani <ptindwani@toth.com>
2011-10-19 18:12:48 -0400

    first commit

---
+++ app/Starbucks/starbucks_controller.rb
@@ -0,0 +1,63 @@
+require 'rho/rhocontroller'
+require 'helpers/browser_helper'
+
+class StarbucksController < Rho::RhoController
+  include BrowserHelper
+
+  # GET /Starbucks
+  def index
+    @starbuckses = Starbucks.find(:all)
+    render
+  end
+
+  # GET /Starbucks/{1}
+  def show
+    @starbucks = Starbucks.find(@params['id'])
+    if @starbucks
+      render :action => :show
+    else
+      redirect :action => :index
+    end
+  end
+
+  # GET /Starbucks/new
+  def new
+    @starbucks = Starbucks.new
+    render :action => :new
+  end
+
+  def choosedrink
+    redirect :action => :drink
+  end
+
+  # GET /Starbucks/{1}/edit
+  def edit
+    @starbucks = Starbucks.find(@params['id'])
+    if @starbucks
+      render :action => :edit
+    else
+      redirect :action => :index
+    end
+  end
+
+  # POST /Starbucks/create
+  def create
+    @starbucks = Starbucks.new(@params['starbucks'])
+    @starbucks.save
+    redirect :action => :index
+  end
+
+  # POST /Starbucks/{1}/update
+  def update
+    @starbucks = Starbucks.find(@params['id'])
+    @starbucks.update_attributes(@params['starbucks']) if @starbucks
+    redirect :action => :index
+  end
+
+  # POST /Starbucks/{1}/delete
+  def delete
+    @starbucks = Starbucks.find(@params['id'])
+    @starbucks.destroy if @starbucks
+    redirect :action => :index  
+  end
+end
